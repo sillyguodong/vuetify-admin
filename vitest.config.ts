974d65e45aef4f66f5a0c74e2012f01586cd1e5e
sillyguodong <33891828+sillyguodong@users.noreply.github.com>
2023-08-14 09:49:15 +0800

    Initial commit

---
+++ vitest.config.ts
@@ -0,0 +1,39 @@
+import { fileURLToPath } from 'node:url';
+
+import vue from '@vitejs/plugin-vue';
+import { configDefaults, defineConfig, mergeConfig } from 'vitest/config';
+
+// @ts-expect-error
+import viteConfig from './vite.config';
+
+/**
+ * Vitest Configure
+ *
+ * @see {@link https://vitest.dev/config/}
+ */
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    plugins: [
+      // @ts-expect-error
+      vue(),
+    ],
+    // Resolver
+    resolve: {
+      // https://vitest.dev/config/#alias
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        '~': fileURLToPath(new URL('./node_modules', import.meta.url)),
+      },
+      extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue'],
+    },
+    test: {
+      environment: 'jsdom',
+      exclude: [...configDefaults.exclude, 'e2e/*'],
+      root: fileURLToPath(new URL('./', import.meta.url)),
+      transformMode: {
+        web: [/\.[jt]sx$/],
+      },
+    },
+  })
+);
